e99257c83cafcd7363b7d38d09382bd71f870c52
Dexter Ketchum <dexterk97@hotmail.com>
2018-03-18 15:11:54 -0400

    Configuration Module for PRAW Login

---
+++ botConfig.py
@@ -0,0 +1,19 @@
+'''
+Written by /u/SIllycore
+
+This module is used as storage for the bot credentials when initializing a Reddit instance.
+The necessary values are hidden from the Github repository to prevent external access to the bot.
+'''
+
+import praw
+
+def login():
+	reddit = praw.Reddit(
+		user_agent = "HIDDEN_AGENT",
+		client_id = "HIDDEN_ID",
+		client_secret = "HIDDEN_SECRET",
+		username = "HIDDEN_USERNAME",
+		password = "HIDDEN_PASSWORD"
+	)
+	
+	return reddit
