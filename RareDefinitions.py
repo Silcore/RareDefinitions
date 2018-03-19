03f309caca2ebb232290303361d23c29b1b8df14
Dexter Ketchum <dexterk97@hotmail.com>
2018-03-19 19:25:28 -0400

    Massive Update (Details Inside)
    - Finalized checkMessages() function. Responds appropriately to "good bot" and "bad bot" inbox messages and replies.
    - Finalized getResponse() function. Generates randomized response based on user sentiment.
    - Added further abstraction of functions.
    - Simplified local diagnostic printouts. Removed "no uncommon words" and "already responded" printouts.
    - Added printouts for start and end of each program subsection (definitions, messages, etc).

--- RareDefinitions.py
+++ RareDefinitions.py
@@ -6,11 +6,15 @@ import botConfig
 import praw
 import os
 import re
+import random
 from nltk.corpus import wordnet
 from nltk.stem.wordnet import WordNetLemmatizer
 from wordfreq import zipf_frequency
 
+# Parses through comments in the designated list of threads. Replies to comments with uncommon words and definitions.
 def defineWords(reddit, threads):
+	print("± Word Definitions Begin ±")
+	
 	for thread in threads:
 		comments = thread.comments.list()
 
@@ -57,27 +61,63 @@ def defineWords(reddit, threads):
 						message += "\n\n***\n"
 				
 				if len(message) > 0:
-					message += "^^Beep! ^^I ^^define ^^uncommon ^^words. ^^| "
-					message += "^^[Github](https://github.com/Silcore/RareDefinitions) ^^| "
-					message += "[^^Message ^^Creator](https://www.reddit.com/message/compose/?to=sillycore)"
+					message += getFooter()
 					comment.reply(message)
-					print("Bot replying to: " + comment.id + " ...")
+					print("\t× Sending definitions to: " + comment.id + " ...")
 					repliedComments.append(comment.id)
-				else:
-					print("Comment " + comment.id + " does not contain rare words. Skipping.")
-			else:
-				print("Comment " + comment.id + " already processed. Skipping.")
 				
 		with open("repliedComments.txt", "w") as file:
 			for commentID in repliedComments:
 				file.write(commentID + '\n')
+				
+	print("± Word Definitions End ±")
 
-'''
+# Checks inbox for "good bot" or "bad bot" messages/replies and responds accordingly.
 def checkMessages(reddit):
-	for message in reddit.inbox.unread():
-		if isinstance(message, Comment):
+	print("± Message Checking Begin ±")
+	
+	unread_messages = []
+	
+	for item in reddit.inbox.unread():
+		# Remove unnecessary symbols to grab only words or fragments.
+		text = re.sub('[^A-Za-z]+', ' ', item.body.lower())
+		
+		if text.find("good bot") != -1 and text.find("bad bot") == -1:
+			item.reply(getResponse("POSITIVE") + "\n\n***\n" + getFooter())
+			print("\t× Sending happy reply to: " + item.id + " ...")
+		elif text.find("bad bot") != -1 and text.find("good bot") == -1:
+			item.reply(getResponse("NEGATIVE") + "\n\n***\n" + getFooter())
+			print("\t× Sending sad reply to: " + item.id + " ...")
+		
+		unread_messages.append(item)
+	
+	reddit.inbox.mark_read(unread_messages)
+	
+	print("± Message Checking End ±")
 
-'''
+# Returns the standard footer for all outgoing bot messages.	
+def getFooter():
+	message = "^^Beep! ^^I'm ^^a ^^friendly ^^robot. ^^| "
+	message += "^^[Github](https://github.com/Silcore/RareDefinitions) ^^| "
+	message += "[^^Message ^^Creator](https://www.reddit.com/message/compose/?to=sillycore)"
+	
+	return message
+
+# Provides a randomized response to "good bot" or "bad bot" messages/replies.
+def getResponse(sentiment):
+	positiveResponses = ["It was my pleasure to serve you. ", "I'm happy to help. ", "You're very welcome! ", "You're very sweet! ",
+						"I'll keep it up! ", "I love to see happy humans! "]
+	negativeResponses = ["I'm sorry to disappoint. ", "I'll be sure to do better next time. ", "I'll try my best to improve. ", "I apologize profusely. ",
+						"Please forgive me, for I'll never forgive myself. ", "Give me a second chance. I'll make it up to you. "]
+	positiveEmojis = ["( ◞･౪･)", "(*\^▽\^*)", "(/\^▽\^)/", "o(≧∇≦o)", "(\^ _ \^)/", "(\*＾ワ＾\*)", "(⊙ヮ⊙)", "＼(\^ω\^＼)"]
+	negativeEmojis = ["(︶︹︺)", "(oꆤ︵ꆤo)", "(▰︶︹︺▰)", "(つ﹏<)･ﾟ｡", "｡ﾟ･（>﹏<）･ﾟ｡", "(´•ω•̥`)", "(•̥́_•ૅू˳)", "(˘̩̩̩ε˘̩ƪ)"]
+	
+	if sentiment == "POSITIVE":
+		return random.choice(positiveResponses) + random.choice(positiveEmojis) + "\n\n"
+	elif sentiment == "NEGATIVE":
+		return random.choice(negativeResponses) + random.choice(negativeEmojis) + "\n\n"
+	
+# Provides a list of top 25 hottest threads from designated subreddit list.
 def getThreads(reddit):
 	# User-defined list of subreddits for the bot to check.
 	subreddits = ["InforMe"]
@@ -87,12 +127,12 @@ def getThreads(reddit):
 		threads.extend(reddit.subreddit(name).hot(limit = 25))
 	
 	return threads
-	
+
 def main():
 	# Initialize Reddit instance and other necessary variables.
 	reddit = botConfig.login()
 	defineWords(reddit, getThreads(reddit))
-	# checkMessages(reddit)
+	checkMessages(reddit)
 	
 	print("Δ The bot has closed successfully. Δ")
 	
