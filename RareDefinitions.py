9cd105f983b6816d7cedcdfe5d3521451966f826
Dexter Ketchum <dexterk97@hotmail.com>
2018-03-18 20:27:40 -0400

    Updated Subreddit Logic
    * The top 25 hottest threads are now pulled from the designated subreddit(s). This allows the program to be run in short bursts over a designated number of threads.
    * Added a print statement for when the program closes successfully.

--- RareDefinitions.py
+++ RareDefinitions.py
@@ -17,55 +17,59 @@ from wordfreq import zipf_frequency
 
 # Initialize Reddit instance and other necessary variables.
 reddit = botConfig.login()
-subreddit = reddit.subreddit("test")
-comments = subreddit.stream.comments()
+hotThreads = reddit.subreddit("test").hot(limit = 25)
 
-if not os.path.isfile("repliedComments.txt"):
-	repliedComments = []
-	with open("repliedComments.txt", "a"): pass
-else:
-	with open("repliedComments.txt", "r") as file:
-		repliedComments = file.read()
-		repliedComments = repliedComments.split()
-		repliedComments = list(filter(None, repliedComments))
+for thread in hotThreads:
+	comments = thread.comments.list()
 
-botComments = reddit.redditor('RareDefinitions').comments.new()
-
-# Iterate through the comments in the designated subreddit(s).
-for comment in comments:
-	# If the comment has not yet been replied to, attempt to do so.
-	if comment.id not in repliedComments and comment.id not in botComments:
-		text = comment.body.lower()
-		message = ""
-	
-		# Remove unnecessary symbols to grab only words or fragments.
-		re.sub('[^A-Za-z]+', ' ', text)
-		
-		# Iterate through each comment body checking for uncommon words.
-		for word in text.split():
-			frequency = zipf_frequency(word, "en", wordlist = "large")
-			if frequency != 0 and frequency < 1.5:
-				message += "> " + '[' + word + "](http://www.dictionary.com/browse/" + word + "?s=t)" + "\n\n"
-				
-				try:
-					synSet = nltk.corpus.wordnet.synsets(word)
-					definition = synSet[0].definition()
-					message += "> " + definition + '\n'
-				except IndexError:
-					message = ""
-					break
-				
-				message += "\n***"
-		
-		if len(message) > 0:
-			message += "^^Beep ^^boop! ^^I'm ^^a ^^bot ^^who ^^replies ^^with ^^definitions ^^for ^^uncommon ^^words. ^^:)\n\n"
-			message += "^^[Github](https://github.com/Silcore/RareDefinitionsBot) ^^| [^^Message ^^Creator](https://www.reddit.com/message/compose/?to=sillycore)"
-			comment.reply(message)
-			print("Bot replying to: " + comment.id + " ...")
-			repliedComments.append(comment.id)
+	if not os.path.isfile("repliedComments.txt"):
+		repliedComments = []
+		with open("repliedComments.txt", "a"): pass
 	else:
-		print("Comment " + comment.id + " already processed. Skipping.")
+		with open("repliedComments.txt", "r") as file:
+			repliedComments = file.read()
+			repliedComments = repliedComments.split()
+			repliedComments = list(filter(None, repliedComments))
+
+	botComments = reddit.redditor('RareDefinitions').comments.new()
+
+	# Iterate through the comments in the designated subreddit(s).
+	for comment in comments:
+		# If the comment has not yet been replied to, attempt to do so.
+		if comment.id not in repliedComments and comment.id not in botComments:
+			text = comment.body.lower()
+			message = ""
 		
-with open("repliedComments.txt", "w") as file:
-	for commentID in repliedComments:
-		file.write(commentID + '\n')
+			# Remove unnecessary symbols to grab only words or fragments.
+			re.sub('[^A-Za-z]+', ' ', text)
+			
+			# Iterate through each comment body checking for uncommon words.
+			for word in text.split():
+				frequency = zipf_frequency(word, "en", wordlist = "large")
+				if frequency != 0 and frequency < 2:
+					message += "> " + '[' + word + "](http://www.dictionary.com/browse/" + word + "?s=t)" + "\n\n"
+					
+					try:
+						synSet = nltk.corpus.wordnet.synsets(word)
+						definition = synSet[0].definition()
+						message += "> " + definition + '\n'
+					except IndexError:
+						message = ""
+						break
+					
+					message += "\n***\n"
+			
+			if len(message) > 0:
+				message += "^^Beep! ^^I ^^define ^^rare ^^words. ^^| "
+				message += "^^[Github](https://github.com/Silcore/RareDefinitionsBot) ^^| [^^Message ^^Creator](https://www.reddit.com/message/compose/?to=sillycore)"
+				comment.reply(message)
+				print("Bot replying to: " + comment.id + " ...")
+				repliedComments.append(comment.id)
+		else:
+			print("Comment " + comment.id + " already processed. Skipping.")
+			
+	with open("repliedComments.txt", "w") as file:
+		for commentID in repliedComments:
+			file.write(commentID + '\n')
+			
+print("Δ Rare Definitions Bot has closed successfully. Δ")
