14ee87323161b3e801abcefa52ec443b12786097
Dexter Ketchum <silcore@hotmail.com>
2018-03-19 23:18:58 -0400

    Revert "Moved getThreads to botConfig from RareDefinitions"
    This reverts commit 77ab2d0d44774e6c0ea524037039f7007b8b1f53.

--- botConfig.py
+++ botConfig.py
@@ -1,5 +1,5 @@
 '''
-Written by /u/SIllycore
+Written by /u/Sillycore
 
 This module is used as storage for the bot credentials when initializing a Reddit instance.
 The necessary values are hidden from the Github repository to prevent external access to the bot.
@@ -17,17 +17,6 @@ def login():
 	)
 	
 	return reddit
-
-# Provides a list of top 25 hottest threads from designated subreddit list.
-def getThreads(reddit):
-	# User-defined list of subreddits for the bot to check.
-	subreddits = ["InforMe"]
-	threads = []
-	
-	for name in subreddits:
-		threads.extend(reddit.subreddit(name).hot(limit = 25))
-	
-	return threads
 
 def getUsername():
 	return "HIDDEN_USERNAME"
